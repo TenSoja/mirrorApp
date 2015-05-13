7ca9b0e17adc3fbc2d4686cbbb6234a19424fe48
Michel <tensoja@gmail.com>
2015-05-13 16:59:36 -0300

    Alterado para aceitar submit pressionando o enter

--- loja/js/sugestoes/sugestoes.js
+++ loja/js/sugestoes/sugestoes.js
@@ -1,10 +1,20 @@
-$("input[type=button]").click(function(event) {
+function adicionaEvento() {
 	if ($("input[type=text]").val() != "") {
 		var sugestao = $("input[type=text]").val();
 		$("ul.sugestoes li:last").clone().addClass('sugestao-inserida').appendTo("ul.sugestoes").find("span:first").text(sugestao);
 	}
+}
+
+$("input[type=button]").click(function(event) {
+	adicionaEvento();
 });
 
 $("ul.sugestoes").on("dblclick", "li", function(event) {
 	$(this).remove();
+});
+
+$(document.body).on('keyup', $("input[type=text]"), function(event) {
+	if (event.keyCode == 13) {
+		adicionaEvento();
+	}
 });
