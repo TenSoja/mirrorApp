0fc5b69089832c4f3ddd526e57eeafc45dbbae1d
Michel <tensoja@gmail.com>
2015-05-13 16:41:25 -0300

    Altera a cor da sugestao adicionada

--- loja/js/sugestoes/sugestoes.js
+++ loja/js/sugestoes/sugestoes.js
@@ -1,7 +1,7 @@
 $("input[type=button]").click(function(event) {
 	if ($("input[type=text]").val() != "") {
 		var sugestao = $("input[type=text]").val();
-		$("ul.sugestoes li:last").clone().appendTo("ul.sugestoes").find("span:first").text(sugestao);
+		$("ul.sugestoes li:last").clone().addClass('sugestao-inserida').appendTo("ul.sugestoes").find("span:first").text(sugestao);
 	}
 });
 
