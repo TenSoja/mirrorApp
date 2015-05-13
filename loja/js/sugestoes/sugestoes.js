56cbc026c1ee1962cfe410527cd1dcc62f2ee9d5
Michel <tensoja@gmail.com>
2015-05-13 16:37:35 -0300

    Altera adição de sugestoes para verificar se o input esta vazio

--- loja/js/sugestoes/sugestoes.js
+++ loja/js/sugestoes/sugestoes.js
@@ -1,8 +1,8 @@
 $("input[type=button]").click(function(event) {
-	var sugestao = $("input[type=text]").val();
-
-	$("ul.sugestoes li:last").clone().appendTo("ul.sugestoes").find("span:first").text(sugestao);
-
+	if ($("input[type=text]").val() != "") {
+		var sugestao = $("input[type=text]").val();
+		$("ul.sugestoes li:last").clone().appendTo("ul.sugestoes").find("span:first").text(sugestao);
+	}
 });
 
 $("ul.sugestoes").on("dblclick", "li", function(event) {
