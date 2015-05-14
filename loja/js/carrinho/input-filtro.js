47d0fef98b13f376f67ad7edaa9826de380436df
Michel <tensoja@gmail.com>
2015-05-14 12:02:52 -0300

    Alterar encapsular em IIFEs as variaveis

--- loja/js/carrinho/input-filtro.js
+++ loja/js/carrinho/input-filtro.js
@@ -1,11 +1,13 @@
-var inputs = document.querySelectorAll("#carrinho input");
-for (var i = 0; i < inputs.length; i++) {
-	inputs[i].addEventListener("input", function(event) {
-		var naoNumericos = /\D/g;
-		this.value = this.value.replace(naoNumericos, 1);
+(function() {
+	var inputs = document.querySelectorAll("#carrinho input");
+	for (var i = 0; i < inputs.length; i++) {
+		inputs[i].addEventListener("input", function(event) {
+			var naoNumericos = /\D/g;
+			this.value = this.value.replace(naoNumericos, "");
 
-	});
-}
+		});
+	}
+})();
 
 /*var $ul = document.querySelector("ul#carrinho");
 $ul.addEventListener("input", function(event) {
