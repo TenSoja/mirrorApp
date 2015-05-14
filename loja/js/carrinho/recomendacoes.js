25b87c7e8563866e4c21a92ef287165ef8371af7
Michel <tensoja@gmail.com>
2015-05-14 10:59:25 -0300

    Desaclopar funcao e adiciona evento a ser observado

--- loja/js/carrinho/recomendacoes.js
+++ loja/js/carrinho/recomendacoes.js
@@ -1,24 +1,30 @@
-var painel = $("#recomendacoes");
-
 setInterval(function() {
 
 	$.getJSON("http://mirrorfashion.caelum.com.br/produtos?callback=?",
 		function(retorno) {
 
-			var ul = $("<ul>");
+			$(document).trigger("novasRecomendacoes", retorno);
 
-			$.each(retorno.produtos, function() {
-				var li = $("<li>");
-				var img = $("<img>").attr("src", this.imagem);
-				var pNome = $("<p>").text(this.nome);
-				var pPreco = $("<p>").text(numberParaReal(this.preco));
+		}, 5000);
 
-				li.append(img).append(pNome).append(pPreco).appendTo(ul);
-			});
+	$(document).on("novasRecomendacoes", function(event, retorno) {
 
-			$("ul", painel).remove();
+		var painel = $("#recomendacoes");
 
-			ul.appendTo(painel);
+		var ul = $("<ul>");
+
+		$.each(retorno.produtos, function() {
+			var li = $("<li>");
+			var img = $("<img>").attr("src", this.imagem);
+			var pNome = $("<p>").text(this.nome);
+			var pPreco = $("<p>").text(numberParaReal(this.preco));
+
+			li.append(img).append(pNome).append(pPreco).appendTo(ul);
 		});
 
-}, 5000);
+		$("ul", painel).remove();
+
+		ul.appendTo(painel);
+	});
+
+});
