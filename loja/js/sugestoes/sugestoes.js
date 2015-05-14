1a1b77e8a57c2cfb0f82bb68a83c48d9828e7a92
Michel <tensoja@gmail.com>
2015-05-14 15:33:03 -0300

    Altera para contabilizar o votos de cada sugestao

--- loja/js/sugestoes/sugestoes.js
+++ loja/js/sugestoes/sugestoes.js
@@ -1,7 +1,28 @@
 function adicionaSugestao() {
 	if ($("input[type=text]").val() != "") {
 		var sugestao = $("input[type=text]").val();
-		$("ul.sugestoes li:last").clone().addClass('sugestao-inserida').appendTo("ul.sugestoes").find("span:first").text(sugestao);
+
+		var sugestaoExistente = $(".sugestoes li").filter(function() {
+			return $(this).find(".sugestao").text() == sugestao;
+		});
+
+		if (sugestaoExistente.length > 0) {
+
+			var votos = sugestaoExistente.data("votos") + 1;
+			sugestaoExistente.data("votos", votos);
+			sugestaoExistente.find(".votos").text(votos + " votos")
+
+		} else {
+			$("ul.sugestoes li:last")
+			.clone()
+			.data("votos", 1)
+			.addClass('sugestao-inserida')
+			.appendTo("ul.sugestoes")
+			.find("span:first")
+			.text(sugestao)
+			.next()
+			.text(1 + " voto");
+		}
 	}
 }
 
