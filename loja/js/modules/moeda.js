4f6440d82bd42a3a63a302cf265c94a127090f0f
Michel <tensoja@gmail.com>
2015-05-14 12:46:11 -0300

    Alterado para utilizar o formato de Module Pattern

--- loja/js/modules/moeda.js
+++ loja/js/modules/moeda.js
@@ -1,11 +1,12 @@
-var formatadorMoeda = {
-	simbolo: "R$ ",
+var formatadorMoeda = (function() {
+	var simbolo = "R$ ";
+	return {
+		numberParaReal : function(numero) {
+			return "R$ " + numero.toFixed(2).replace(".", ",");
+		},
 
-	numberParaReal: function(numero) {
-		return "R$ " + numero.toFixed(2).replace(".", ",");
-	},
-
-	realParaNumber: function(texto) {
-		return parseFloat(texto.replace("R$ ", "").replace(",", "."));
-	}
-};
+		realParaNumber : function(texto) {
+			return parseFloat(texto.replace("R$ ", "").replace(",", "."));
+		}
+	};
+})();
