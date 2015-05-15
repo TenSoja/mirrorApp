3a6e5c27b1b12bf3647f6bede4ab191957d96e96
Michel <tensoja@gmail.com>
2015-05-15 14:03:37 -0300

    Altera para receber o seletor como parametro

--- loja/js/lib/input-plugin.js
+++ loja/js/lib/input-plugin.js
@@ -1,13 +1,14 @@
 (function($) {
 	"use strict";
 
-	$.fn.inputNumerico = function() {
+	$.fn.inputNumerico = function(seletor) {
 
-		return this.on("input", function(event) {
+		return this.on("input", seletor, function(event) {
 			var naoNumeros = /\D/g;
 			if(naoNumeros.test(this.value)) {
 				this.value = this.value.replace(naoNumeros, "");
+				$(this).trigger("input");
 			}
-		})
-	};
+		});
+	}
 })(jQuery);
